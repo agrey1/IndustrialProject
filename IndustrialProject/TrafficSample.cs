212dd88a0e36ab62fafd5de8c204bba897fcf7b4
Alexander Grey <alexandergrey@hotmail.co.uk>
2016-09-15 11:46:41 +0100

    Added search for sequence number in packet data

--- IndustrialProject/TrafficSample.cs
+++ IndustrialProject/TrafficSample.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +22,50 @@ namespace IndustrialProject
 
         public void setPackets(List<Packet> packets)
         {
+            int sequencePosition = -1;
+            
+            //Attempt to find a sequence number
+            for(int i = 0; i < packets.Count; i++)
+            {
+                //Check each byte in the packet until we find it
+                List<int> bytes = packets[i].getBytes();
+                for (int j = 0; j < bytes.Count; j++)
+                {
+                    //Look for incrementing bytes in the next 5 packets
+                    int sequence = bytes[j];
+                    for(int k = 0; k < 5; k++)
+                    {
+                        if(i + k < packets.Count)
+                        {
+                            if(packets[i + k].getBytes()[j] == sequence + 1)
+                            {
+                                if(k == 4)
+                                {
+                                    //Sequence number found
+                                    sequencePosition = j;
+                                    break;
+                                }
+
+                                sequence++;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (sequencePosition != -1) break;
+                }
+
+                if (sequencePosition != -1) break;
+            }
+
+            foreach(Packet packet in packets)
+            {
+                packet.setSequenceNumber(packet.getBytes()[sequencePosition]);
+            }
+
             this.packets = packets.ToList();
         }
 
