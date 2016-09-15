1cc0351c213835842052eb0ef36961e7983ed8ae
Alexander Grey <alexandergrey@hotmail.co.uk>
2016-09-15 15:43:54 +0100

    nextErrorButton click handler, setting Packet.outOfSequence error boolean

--- IndustrialProject/Form1.cs
+++ IndustrialProject/Form1.cs
@@ -23,7 +23,7 @@ namespace IndustrialProject
         {
             InitializeComponent();
         }
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
             //webBrowser1.Navigate(@"C:\Users\AlexanderGrey\Documents\Visual Studio 2013\Projects\IndustrialProject\IndustrialProject\bin\Debug\test.html");
@@ -34,7 +34,7 @@ namespace IndustrialProject
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            System.Drawing.Icon myIcon = new System.Drawing.Icon(System.Drawing.SystemIcons.Information,32,32);
+            System.Drawing.Icon myIcon = new System.Drawing.Icon(System.Drawing.SystemIcons.Information, 32, 32);
             aboutToolStripMenuItem.Image = myIcon.ToBitmap();
 
             dataRateOverTimeToolStripMenuItem.Checked = true;
@@ -45,7 +45,7 @@ namespace IndustrialProject
             string[] columns = { "Time", "Address", "Port", "Sequence Number", "Protocol", "Length", "Errors" };
             ColumnHeader columnHeader;
 
-            foreach(string column in columns)
+            foreach (string column in columns)
             {
                 columnHeader = new ColumnHeader();
                 columnHeader.Text = column;
@@ -55,8 +55,13 @@ namespace IndustrialProject
             backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
             backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
             backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+
+            while (linePanels.Count < tabControl1.TabCount)
+            {
+                linePanels.Add(new List<Panel>());
+            }
         }
-        
+
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog packetCaptureDialog = new OpenFileDialog();
@@ -119,9 +124,13 @@ namespace IndustrialProject
             }
             else
             {
+                tabControl1.SelectedIndex = tabControl1.TabCount - 1;
+
+                /*
                 packetContentTextBox.AppendText("File start time: " + sample.getStartTime().ToString() + "\n");
                 packetContentTextBox.AppendText("File end time: " + sample.getEndTime().ToString() + "\n");
                 packetContentTextBox.AppendText("File source port: " + sample.getSourcePort() + "\n");
+                */
 
                 int count = 0;
                 foreach (Packet packet in sample.getPackets())
@@ -133,11 +142,13 @@ namespace IndustrialProject
                     startTimeLabel.Text = sample.getStartTime().ToString();
                     endTimeLabel.Text = sample.getEndTime().ToString();
 
+                    /*
                     packetContentTextBox.AppendText("Packet:\n");
                     packetContentTextBox.AppendText("Time: " + packet.getTime().ToString() + " " + packet.getTime().Millisecond.ToString() + "\n");
                     packetContentTextBox.AppendText("Data: " + packet.getByteStr() + "\n");
                     packetContentTextBox.AppendText("EEP: " + packet.getEEP().ToString() + "\n");
                     packetContentTextBox.AppendText("None: " + packet.getNone().ToString() + "\n");
+                    */
 
                     //"Time", "Address", "Port", "Sequence Number", "Protocol", "Length", "Errors"
                     ListViewItem item = new ListViewItem();
@@ -164,7 +175,7 @@ namespace IndustrialProject
                     {
                         errorStr += "None, ";
                     }
-                    if(packet.getInvalidAddress() == true)
+                    if (packet.getInvalidAddress() == true)
                     {
                         errorStr += "Invalid Address, ";
                     }
@@ -173,6 +184,7 @@ namespace IndustrialProject
                         if (packets[count - 1].getSequenceNumber() != packet.getSequenceNumber() - 1)
                         {
                             errorStr += "Out of sequence, ";
+                            packet.setOutOfSequence(true);
                         }
                     }
 
@@ -182,7 +194,7 @@ namespace IndustrialProject
                     }
                     subItems[5].Text = errorStr;
 
-                    if(errorStr != "")
+                    if (errorStr != "")
                     {
                         item.BackColor = Color.Red;
 
@@ -211,10 +223,12 @@ namespace IndustrialProject
                     {
                         item.SubItems.Add(subItem);
                     }
+                       
 
                     packetListView.Items.Add(item);
                     count++;
                 }
+
 
                 //Todo: Display average data rate (After data rate has been found)
             }
@@ -273,6 +287,53 @@ namespace IndustrialProject
                         panel.Hide();
                     }
                 }
+            }
+        }
+
+        private void packetListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (packetListView.SelectedIndices.Count > 0)
+            {
+                packetContentTextBox.Text = sample.getPackets()[packetListView.SelectedIndices[0]].getHexStr();
+            }
+        }
+
+        private void nextErrorButton_Click(object sender, EventArgs e)
+        {
+            int current = 0;
+            if(packetListView.SelectedIndices.Count > 0)
+            {
+                current = packetListView.SelectedIndices[0];
+
+                if(current == sample.getPackets().Count - 1)
+                {
+                    current = 0;
+                }
+            }
+
+            if (sample != null)
+            {
+                for (int i = current + 1; i < sample.getPackets().Count; i++)
+                {
+                    if (sample.getPackets()[i].hasError())
+                    {
+                        packetListView.Items[i].Selected = true;
+                        packetListView.Items[i].Focused = true;
+                        packetListView.EnsureVisible(i);
+                        packetListView.Select();
+
+                        break;
+                    }
+
+                    if(i == sample.getPackets().Count - 1)
+                    {
+                        MessageBox.Show("The currently open traffic sample does not contain any errors.", "No errors found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please open a traffic recording in order to view the errors contained within.", "No errors to view", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
     }
