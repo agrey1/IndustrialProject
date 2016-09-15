786ce75f70e7328c8f33afee49f9bc4b1119b0f5
Harry Adam <Har.a@hotmail.co.uk>
2016-09-15 11:30:10 +0100

    Must commit to update project

--- IndustrialProject/Form1.cs
+++ IndustrialProject/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IndustrialProject
@@ -207,6 +202,11 @@ namespace IndustrialProject
         private void viewToolStripMenuItem1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Navigate(@"C:\Users\Harry\Dropbox\IndustrialTeamProject\Harry\index.html");
         }
     }
 }
